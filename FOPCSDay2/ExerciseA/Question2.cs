4bb1accfcc70b3db1228e32e1897941ee340c760
Xinyi <xinzwerkz@gmail.com>
2021-08-05 23:05:30 +0800

    * FOPCSDay2.sln: * Question2.cs: * UserPrefs.xml: * Quiz.dll: * Quiz.pdb: * Quiz.dll: * ExerciseA-Debug.json: * Quiz.genruntimeconfig.cache: * Quiz.csproj.FileListAbsolute.txt: * Quiz.csproj.AssemblyReference.cache: Add solution for question 2   Exercise A
    * Quiz.pdb: Add solution for question 2 Exercise A

--- FOPCSDay2/ExerciseA/Question2.cs
+++ FOPCSDay2/ExerciseA/Question2.cs
@@ -3,11 +3,13 @@ namespace ExerciseA
 {
     public class Question2
     {
-        //public Question2()
-        //{
-        static void Main(string[] args)
+        public Question2()
         {
-
+            //static void Main(string[] args)
+            //{
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("\tGood Morning " + name);
         }
         }
 }
