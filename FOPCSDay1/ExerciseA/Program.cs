ab9eaa3db1a1b0991bfaf9cbdf13912514e4390e
Xinyi <xinzwerkz@gmail.com>
2021-08-05 14:24:04 +0800

    Add Day1 lesson files

---
+++ FOPCSDay1/ExerciseA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciseA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
