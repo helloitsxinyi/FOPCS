9036b0b58769c2c657e90e19b58c8846aebd7251
Xinyi <xinzwerkz@gmail.com>
2021-08-05 14:24:19 +0800

    Add Day2 files

---
+++ FOPCSDay2/ExerciseA/Question2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ExerciseA
+{
+    public class Question2
+    {
+        //public Question2()
+        //{
+        static void Main(string[] args)
+        {
+
+        }
+        }
+}
