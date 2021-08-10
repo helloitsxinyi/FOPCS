8b97985f35f4fb7da957453f8725c72a788c64a5
Xinyi <xinzwerkz@gmail.com>
2021-08-10 10:17:54 +0800

    Add solution for Day 3 quiz

---
+++ FOPCSDay3/Quiz/Day3Quiz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Day3Quiz
+    {
+        static void Main(string[] args)
+        {
+            int price = 500;
+            Console.WriteLine("Welcome to ISS Gadget Shop!");
+            Console.WriteLine("Number of items to purchase: ");
+            int quantity = int.Parse(Console.ReadLine());
+            double discount;
+
+            // is it recommended to store d qty * price as another variable? might take up more memory?
+            // ANS: recommended to create. so that the coding is clearer.
+            // expresions in conditional statement, syntax is correct but less readable
+            if (quantity * price <= 2000)
+            {
+                discount = 0;
+            }
+            else if (quantity * price <= 3000)
+            {
+                discount = 0.03;
+            }
+            else if (quantity * price <= 6000)
+            {
+                discount = 0.05;
+            }
+            else
+            {
+                discount = 0.08;
+            }
+            double payment = (1 - discount) * (quantity * price);
+            Console.WriteLine($"Please pay ${payment:0,0.00}");
+        }
+    }
+}
