423432ad7b7d67642724c9be163210dd1d51acc0
Xinyi <xinzwerkz@gmail.com>
2021-08-11 22:51:40 +0800

    Add ExerciseE menu

---
+++ FOPCSDay3/ExerciseE/ExerciseE.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExerciseE
+{
+    class ExerciseE
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the question you would like to test: ");
+            string question = Console.ReadLine();
+
+            switch (question)
+            {
+                case "1":
+                    Q1.solution();
+                    break;
+                case "2":
+                    Q2.solution();
+                    break;
+                case "3":
+                    Q3.solution();
+                    break;
+                case "4":
+                    Q4.solution();
+                    break;
+                case "5":
+                    Q5.solution();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a number from 1 to 5!");
+                    Main();
+                    break;
+            }
+        }
+    }
+}
