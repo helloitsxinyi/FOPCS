874bd8c22fd237ec70d23f1b54994640cbe56ac3
Xinyi <xinzwerkz@gmail.com>
2021-08-11 22:53:04 +0800

    Add Quiz menu for FOPCSDay4

--- FOPCSDay4/Quiz/Program.cs
+++ FOPCSDay4/Quiz/Program.cs
@@ -4,9 +4,33 @@ namespace FOPCSDay4
 {
     class Quiz
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the question you would like to test: ");
+            string question = Console.ReadLine();
+
+            switch (question)
+            {
+                case "1":
+                    Q1.solution();
+                    break;
+                case "2":
+                    Q2.solution();
+                    break;
+                case "3":
+                    Q3.solution();
+                    break;
+                case "4":
+                    Q4.solution();
+                    break;
+                case "5":
+                    Q5.solution();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a number from 1 to 5!");
+                    Main();
+                    break;
+            }
         }
     }
 }
