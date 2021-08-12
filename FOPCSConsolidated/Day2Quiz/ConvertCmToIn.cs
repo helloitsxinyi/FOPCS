9686e19569859cb32f57580842b3b400b0ac7486
Xinyi Tay <65388923+xinyitay@users.noreply.github.com>
2021-08-12 23:21:10 +0800

    Add files via upload

---
+++ FOPCSConsolidated/Day2Quiz/ConvertCmToIn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Quiz
+{
+    class ConvertCmToIn
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the length in cm: ");
+            double lengthInCm = double.Parse(Console.ReadLine());
+            double lengthInInch = lengthInCm / 2.54;
+            Console.WriteLine("{0:0.000}", lengthInInch);
+
+
+        }
+    }
+}
