e79fa387c08c75e20a2f474442efc8acef717b46
Xinyi <xinzwerkz@gmail.com>
2021-08-12 14:43:29 +0800

    Add solution for Q2

--- FOPCSDay3/ExerciseE/Q2.cs
+++ FOPCSDay3/ExerciseE/Q2.cs
@@ -5,7 +5,20 @@ namespace ExerciseE
     {
         public static void solution()
         {
-            Console.WriteLine("hello");
+          
+            for (double num = 1; num <= 10; num++)
+            {
+
+                double inverse = 1 / num;
+                double root = Math.Sqrt(num);
+                double square = Math.Pow(num, 2);
+
+                Console.WriteLine($"Number{num: 0.0}\tInverse{inverse: 0.0##}\tSquare root{root: 0.0##}\tSquare {square: 0.0}");
+          
+            }
+
+
+            ExerciseE.Main();
         }
     }
 }
