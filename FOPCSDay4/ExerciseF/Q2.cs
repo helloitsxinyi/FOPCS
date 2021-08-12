6111e3cfc2775536fecc5cdcfaf1858d46f05eb5
Xinyi <xinzwerkz@gmail.com>
2021-08-12 22:34:26 +0800

    Add Q1-6 files for ExerciseF

---
+++ FOPCSDay4/ExerciseF/Q2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ExerciseE
+{
+    public class Q2
+    {
+        public static void solution()
+        {
+            Console.WriteLine("NO\tINVERSE\t\tSQUARE ROOT\tSQUARE");
+            Console.WriteLine("------------------------------------------------------");
+            
+            for (double num = 1; num <= 10; num++)
+            {
+
+                double inverse = 1 / num;
+                double root = Math.Sqrt(num);
+                double square = Math.Pow(num, 2);
+
+                Console.WriteLine($"{num: 0.0}\t{inverse: 0.0##}\t\t{root: 0.0##}\t\t{square: 0.0}");
+          
+            }
+
+
+            ExerciseE.Main();
+        }
+    }
+}
---
+++ FOPCSDay4/ExerciseF/Q3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ExerciseE
+{
+    public class Q3
+    {
+        public static void solution()
+        { 
+            // v elegant solution!
+            Console.WriteLine("Please enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            int divisorCount;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if num
+            }
+        }
+    }
+}
