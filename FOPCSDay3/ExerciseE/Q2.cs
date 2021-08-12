6316104a632eb223ce773d042f725cf4f0bfba34
Xinyi <xinzwerkz@gmail.com>
2021-08-12 22:53:05 +0800

    * Q2.cs: Clean up code in EQ2 and add solution for EQ3 * ExerciseE.csproj.AssemblyReference.cache: Clean up code in EQ2 and add solution for EQ3
    * Q3.cs: Clean up code in EQ2 and add solution for EQ3

--- FOPCSDay3/ExerciseE/Q2.cs
+++ FOPCSDay3/ExerciseE/Q2.cs
@@ -19,7 +19,6 @@ namespace ExerciseE
           
             }
 
-
             ExerciseE.Main();
         }
     }
--- FOPCSDay3/ExerciseE/Q3.cs
+++ FOPCSDay3/ExerciseE/Q3.cs
@@ -5,7 +5,29 @@ namespace ExerciseE
     {
         public static void solution()
         {
-            Console.WriteLine("hello");
+            // v elegant solution!
+            Console.WriteLine("Please enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            int divisorCount = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    divisorCount += 1;
+                }
+            }
+
+            if (divisorCount > 2)
+            {
+                Console.WriteLine("Your number is not a prime number.");
+            }
+            else
+            {
+                Console.WriteLine("Your number is a prime number!");
+            }
+
+            ExerciseE.Main();
         }
     }
 }
