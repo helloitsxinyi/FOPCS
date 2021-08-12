30b4051f79b02e5fc5621019ba330e411c966a60
Xinyi Tay <65388923+xinyitay@users.noreply.github.com>
2021-08-12 23:10:34 +0800

    Add consolidated FOPCS files

---
+++ FOPCSConsolidated/FOPCSDay1/FirstProgram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FOPCS
+{
+    class FirstProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
+        }
+    }
+}
