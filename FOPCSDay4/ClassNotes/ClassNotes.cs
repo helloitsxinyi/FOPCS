7b0b29b1f8cddb4b5f764cbbe655b456772d1e6f
Xinyi Tay <65388923+xinyitay@users.noreply.github.com>
2021-08-12 10:58:32 +0800

    Add ExerciseF and ClassNotes projects

---
+++ FOPCSDay4/ClassNotes/ClassNotes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassNotes
+{
+    class ClassNotes
+    {
+        static void Main(string[] args)
+        {
+            string a = "bbc";
+            string b = "Cac";
+            Console.WriteLine(a.CompareTo(b));
+            Console.WriteLine(b.CompareTo(a));
+        }
+    }
+}
---
+++ FOPCSDay4/ExerciseF/ExerciseF.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciseF
+{
+    class ExerciseF
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
