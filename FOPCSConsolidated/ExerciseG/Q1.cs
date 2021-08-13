2625d6cbf529fbaee9caaf111c7f12af83df4141
Xinyi <xinzwerkz@gmail.com>
2021-08-13 15:22:41 +0800

    Add solution for entire Q1

--- FOPCSConsolidated/ExerciseG/Q1.cs
+++ FOPCSConsolidated/ExerciseG/Q1.cs
@@ -5,29 +5,62 @@ namespace ExerciseG
     {
         public static void solution()
         {
-            Console.WriteLine("Enter a number: ");
-            int num = int.Parse(Console.ReadLine());
+           double[] sales = new double[12];
 
-         
-            while (num < 0)
+            // partA
+            for (int month = 0; month < 12; month++)
+            { 
+                //need to handle exception for invalid sales input (Type error)
+                Console.Write($"Please enter the sales of month {month + 1}: ");
+                sales[month] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Here are the sales of all 12 months that you have entered: ");
+
+            // partB
+            for (int i = 0; i < 12; i++)
             {
-                Console.WriteLine("Error! Please input a number that is 0 or larger.");
-                Console.WriteLine("Enter a number: ");
-                num = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Month {i + 1}: {sales[i]}");
             }
 
-            if (num >= 0)
-            {
-                int answer = 1;
+            int maxMonth = 0;
+            double maxMonthSales = sales[0];
 
-                for (int i = num; i >= 1; i--)
-                {
-                    answer *= i;
+            for (int i = 0; i < 12; i++)
+                if (sales[i] > maxMonthSales) {
+                    maxMonth = i + 1;
+                    maxMonthSales = sales[i];
                 }
+            Console.WriteLine($"The month with max sales is: Month {maxMonth}.");
 
-                Console.WriteLine("The factorial of " + num + " is " + answer);
-                ExerciseG.Main();
+            // partC
+            int minMonth = 0;
+            double minMonthSales = sales[0];
+
+            for (int i = 0; i < 12; i++)
+                if (sales[i] < minMonthSales)
+                {
+                    minMonth = i + 1;
+                    minMonthSales = sales[i];
+                }
+            Console.WriteLine($"The month with min sales is: Month {minMonth}.");
+
+
+            // partD
+            double totalSales = 0;
+
+            for (int i = 0; i < 12; i++)
+            { 
+                totalSales += sales[i];
+               
             }
+
+            double average = totalSales / sales.GetLength(0);
+            Console.WriteLine($"The average monthly sales is {average}.");
+
+
+
+            ExerciseG.Main();
+            
         } 
     }
 }
