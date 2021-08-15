9b664f6b9bf8064d30bea623fafc0746b387821f
Xinyi <xinzwerkz@gmail.com>
2021-08-16 00:28:09 +0800

    Complete solution for HQ1

--- FOPCSConsolidated/ExerciseH/Q1.cs
+++ FOPCSConsolidated/ExerciseH/Q1.cs
@@ -5,23 +5,29 @@ namespace ExerciseH
     {
         public static void solution()
         {
+            Console.Write("Enter an integer:");
+            string message = Console.ReadLine();
+            ReadInteger(message);
+            ExerciseH.Main();            
+        }
+
+        public static int ReadInteger(string message)
+        {
+            int parsed = 0;
             try
             {
-                Console.Write("Enter an integer:");
-                string message = Console.ReadLine();
-                ReadInteger(message);
-
+                parsed = int.Parse(message);
+                Console.WriteLine($"Your integer is {parsed}!");
             }
             catch (FormatException e)
             {
                 Console.WriteLine("Invalid input! Please enter an integer: ");
                 message = Console.ReadLine();
+                ReadInteger(message);
             }
-          
 
-            ExerciseH.Main();
-            
-        } 
+            return parsed;
+        }
     }
 }
 
