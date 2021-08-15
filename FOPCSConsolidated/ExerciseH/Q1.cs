186b4e7d5e05eac8dd7900bd897cf900d532dccf
Xinyi <xinzwerkz@gmail.com>
2021-08-16 00:06:16 +0800

    Add HQ1 solution

---
+++ FOPCSConsolidated/ExerciseH/Q1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ExerciseH
+{
+    public class Q1
+    {
+        public static void solution()
+        {
+            try
+            {
+                Console.Write("Enter an integer:");
+                string message = Console.ReadLine();
+                ReadInteger(message);
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid input! Please enter an integer: ");
+                message = Console.ReadLine();
+            }
+          
+
+            ExerciseH.Main();
+            
+        } 
+    }
+}
+
