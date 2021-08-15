095395f2b0dd1dd71a64b3f7f78f6331dfda546a
Xinyi <xinzwerkz@gmail.com>
2021-08-16 00:47:47 +0800

    Add solution for HQ2

---
+++ FOPCSConsolidated/ExerciseH/Q2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace ExerciseH
+{
+    public class Q2
+    {
+        public static void solution()
+        {            
+            try
+            {
+                Console.WriteLine("Enter the length of your array: ");
+                int length = int.Parse(Console.ReadLine());
+                int[] arr = new int[length];
+
+                Console.WriteLine("Enter the values of your array: ");
+
+                for (int i = 0; i < length; i++)
+                {
+                    Console.WriteLine($"Enter the value of the number {i + 1} element: ");
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+
+                PrintArray(arr);
+            
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid input! Please input an integer.");
+                Q2.solution();
+            }
+
+          
+
+        }
+
+        private static void PrintArray(int[] arr)
+        {            
+            Console.WriteLine("Your array is [{0}]", string.Join(",", arr));
+        }
+                       
+    }
+}
+
