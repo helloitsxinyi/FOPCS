358911bd04bce0a631b61cc9f1ac44e85fa53df1
Xinyi <xinzwerkz@gmail.com>
2021-08-16 00:31:29 +0800

    Update GQ1 to include try-catch handler for invalid input exception

--- FOPCSConsolidated/ExerciseG/Q1.cs
+++ FOPCSConsolidated/ExerciseG/Q1.cs
@@ -5,63 +5,71 @@ namespace ExerciseG
     {
         public static void solution()
         {
-           double[] sales = new double[12];
+            double[] sales = new double[12];
 
             // partA
-            for (int month = 0; month < 12; month++)
-            { 
-                //need to handle exception for invalid sales input (Type error)
-                Console.Write($"Please enter the sales of month {month + 1}: ");
-                sales[month] = double.Parse(Console.ReadLine());
-            }
-            Console.WriteLine("Here are the sales of all 12 months that you have entered: ");
-
-            // partB
-            for (int i = 0; i < 12; i++)
+            try
             {
-                Console.WriteLine($"Month {i + 1}: {sales[i]}");
-            }
-
-            int maxMonth = 0;
-            double maxMonthSales = sales[0];
-
-            for (int i = 0; i < 12; i++)
-                if (sales[i] > maxMonthSales) {
-                    maxMonth = i + 1;
-                    maxMonthSales = sales[i];
-                }
-            Console.WriteLine($"The month with max sales is: Month {maxMonth}.");
-
-            // partC
-            int minMonth = 0;
-            double minMonthSales = sales[0];
-
-            for (int i = 0; i < 12; i++)
-                if (sales[i] < minMonthSales)
+                for (int month = 0; month < 12; month++)
                 {
-                    minMonth = i + 1;
-                    minMonthSales = sales[i];
+                    Console.Write($"Please enter the sales of month {month + 1}: ");
+                    sales[month] = double.Parse(Console.ReadLine());
                 }
-            Console.WriteLine($"The month with min sales is: Month {minMonth}.");
+                Console.WriteLine("Here are the sales of all 12 months that you have entered: ");
+
+                for (int i = 0; i < 12; i++)
+                {
+                    Console.WriteLine($"Month {i + 1}: {sales[i]}");
+                }
 
 
-            // partD
-            double totalSales = 0;
+                // partB
+                int maxMonth = 0;
+                double maxMonthSales = sales[0];
 
-            for (int i = 0; i < 12; i++)
-            { 
-                totalSales += sales[i];
-               
+                for (int i = 0; i < 12; i++)
+                    if (sales[i] > maxMonthSales)
+                    {
+                        maxMonth = i + 1;
+                        maxMonthSales = sales[i];
+                    }
+                Console.WriteLine($"The month with max sales is: Month {maxMonth}.");
+
+                // partC
+                int minMonth = 0;
+                double minMonthSales = sales[0];
+
+                for (int i = 0; i < 12; i++)
+                    if (sales[i] < minMonthSales)
+                    {
+                        minMonth = i + 1;
+                        minMonthSales = sales[i];
+                    }
+                Console.WriteLine($"The month with min sales is: Month {minMonth}.");
+
+
+                // partD
+                double totalSales = 0;
+
+                for (int i = 0; i < 12; i++)
+                {
+                    totalSales += sales[i];
+
+                }
+
+                double average = totalSales / sales.GetLength(0);
+                Console.WriteLine($"The average monthly sales is {average: 0,0,0.00}.");
+
+                ExerciseG.Main();
+
             }
 
-            double average = totalSales / sales.GetLength(0);
-            Console.WriteLine($"The average monthly sales is {average}.");
-
-
-
-            ExerciseG.Main();
-            
-        } 
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid input! Please try again.");
+                Q1.solution();
+            }
+        }
     }
 }
 
